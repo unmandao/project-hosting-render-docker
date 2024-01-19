42e901667468bea0834079eaf6f1c183b9743f40
unmandao <dbausela@gmail.com>
2024-01-19 20:58:16 +0100

    usando secrets

--- src/main/java/com/crud/project/controller/HelloController.java
+++ src/main/java/com/crud/project/controller/HelloController.java
@@ -1,22 +1,20 @@
 package com.crud.project.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.crud.project.repository.ProductoRepository;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
     
-    @Autowired
-    public ProductoRepository productoRepository;
+    @Value("${MI_ENV_VARIABLE}")
+    private String miEnvVariable;
 
     @GetMapping(value ="/world")
     public String getAllProductos(){
-        return "Hello world";
+        return "Hello world: " + miEnvVariable;
     }
 
 }
